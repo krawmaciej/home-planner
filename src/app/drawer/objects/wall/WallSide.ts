20aa5f4c7ccf0807b4fea8c52e1b7eaca31ca884
Maciej Krawczyk <krawmaciej@gmail.com>
2022-03-28 22:28:16 +0200

    Done linked list

--- src/app/drawer/objects/wall/WallSide.ts
+++ src/app/drawer/objects/wall/WallSide.ts
@@ -1,31 +1,87 @@
-import { Vector3 } from "three";
+import { BufferGeometry, Line, Material, Vector3 } from "three";
 import WallComponent from "./WallComponent";
+import { WallSideType } from "./WallSides";
 
 
 export default class WallSide {
 
     private readonly head: SideNode;
     private readonly tail: SideNode;
+    private readonly side: WallSideType;
+    private readonly strategyKey: "x" | "z"; // 'strategy' used for finding where to put point
     
-    public constructor(start: Vector3, end: Vector3) {
+    public constructor(start: Vector3, end: Vector3, side: WallSideType) {
         this.head = new SideNode(start);
         this.tail = new SideNode(end);
         this.head.connection = new Connection(this.tail, ConnectionType.SOLID); // connect
+        this.side = side;
+        const xStrategy = side === WallSideType.BOTTOM || side === WallSideType.TOP;
+        this.strategyKey = xStrategy ? "x" : "z";
     }
 
+    public createDrawableObjects(material: Material): Array<Line> {
+        let iterator: SideNode = this.head;
+        const lines = new Array<Line>();
+        while (iterator.connection.next !== undefined) {
+            if (iterator.connection.type === ConnectionType.SOLID) {
+                const p0 = iterator.point;
+                const p1 = iterator.connection.next.point;
+                const bg = new BufferGeometry().setFromPoints([p0, p1]);
+                lines.push(new Line(bg, material));
+            }
+            iterator = iterator.connection.next;
+        }
+        return lines;
+    }
+
+    /**
+     * 
+     * @param p0 has to be smaller than {@link this.tail.point}
+     * @param p1 has to be smaller or equal to {@link this.tail.point}
+     */
     public cutBlock(p0: Vector3, p1: Vector3) {
         const first = new SideNode(p0);
-        const last = new SideNode(p1);
-        // handle logic when p0 === start etc.
+        const second = new SideNode(p1);
+
+        const strategyKey = this.strategyKey; // 'alias'
+
+        // check first pair
+        let beforeIterator: SideNode = this.head;
+        let iterator: SideNode | undefined = this.head.connection.next;
+
+        while (iterator !== undefined) {
+            if (p1[strategyKey] < iterator.point[strategyKey]) { // found higher
+                // cut between iterator and beforeIterator
+                if (p0[strategyKey] === beforeIterator.point[strategyKey]) {
+                    beforeIterator.connection = new Connection(second, ConnectionType.HOLE);
+                    second.connection = new Connection(iterator, ConnectionType.SOLID);
+                } else {
+                    beforeIterator.connection.next = first;
+                    first.connection = new Connection(second, ConnectionType.HOLE);
+                    second.connection = new Connection(iterator, ConnectionType.SOLID);
+                }
+                break;
+            } else if (p1[strategyKey] === iterator.point[strategyKey]) { // second point is same as existing
+                if (p0[strategyKey] === beforeIterator.point[strategyKey]) { // swap current solid line for hole
+                    beforeIterator.connection.type = ConnectionType.HOLE;
+                } else {
+                    beforeIterator.connection.next = first;
+                    first.connection = new Connection(iterator, ConnectionType.HOLE);
+                }
+                break;
+            }
+            beforeIterator = iterator;
+            iterator = iterator.connection.next; // go to next node
+        }
     }
 }
 
 class SideNode {
     public readonly point: Vector3;
-    public connection: Connection | undefined;
+    public connection: Connection;
     public constructor(point: Vector3) {
         this.point = point;
-        this.connection = undefined;
+        this.connection = new Connection(undefined, ConnectionType.TAIL);
     }
 }
 
--- src/app/drawer/objects/wall/WallSides.ts
+++ src/app/drawer/objects/wall/WallSides.ts
@@ -1,4 +1,4 @@
-import { Points, Vector3 } from "three";
+import { Line, Material, Points, Vector3 } from "three";
 import { WallPoint } from "../../components/DrawerMath";
 import WallSide from "./WallSide";
 
@@ -9,25 +9,34 @@ export type WallPoints = {
     points: [Vector3, Vector3, Vector3, Vector3]
 }
 
+export enum WallSideType {
+    TOP, RIGHT, BOTTOM, LEFT
+}
+
 /**
  * Wraps map of wall sides.
  */
 export default class WallSides {
-    // map/array keys/indices
-    public static readonly TOP = "TOP";
-    public static readonly RIGHT = "RIGHT";
-    public static readonly BOTTOM = "BOTTOM";
-    public static readonly LEFT = "LEFT";
+    private readonly map: Map<WallSideType, WallSide>;
 
-    private readonly map: Map<string, WallSide>;
-
-    private constructor(map: Map<string, WallSide>) {
-        this.map = map;
+    public constructor(points: WallPoints) {
+        this.map = new Map<WallSideType, WallSide>();
+        this.putWallSides(points);
     }
 
-    public static createFromPoints(points: Points): WallSides {
-        const map = new Map<string, WallSide>();
-        map.set()
+    public createDrawableObjects(material: Material): Array<Line> {
+        return Array.from(this.map.values()).flatMap(value => value.createDrawableObjects(material));
     }
-    
+
+    private putWallSides({ points }: WallPoints): void {
+        this.putWallSide(points[WallPoint.TOP_LEFT], points[WallPoint.TOP_RIGHT], WallSideType.TOP);
+        this.putWallSide(points[WallPoint.BOTTOM_LEFT], points[WallPoint.BOTTOM_RIGHT], WallSideType.BOTTOM);
+        this.putWallSide(points[WallPoint.BOTTOM_LEFT], points[WallPoint.TOP_LEFT], WallSideType.LEFT);
+        this.putWallSide(points[WallPoint.BOTTOM_RIGHT], points[WallPoint.TOP_RIGHT], WallSideType.RIGHT);
+    }
+
+    private putWallSide(p0: Vector3, p1: Vector3, type: WallSideType) {
+        const wallSide = new WallSide(p0, p1, type);
+        this.map.set(type, wallSide);
+    }
 }
