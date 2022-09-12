8517834ae7c943fa54e0a9110b34fc7f13f9ca71
Maciej Krawczyk <krawmaciej@gmail.com>
2022-09-12 18:55:53 +0200

    Fix wall component orientation

--- src/app/arranger/objects/ImportedObject.ts
+++ src/app/arranger/objects/ImportedObject.ts
@@ -3,4 +3,7 @@ import {ArrangerObject} from "./ArrangerObject";
 export type ObjectProps = ArrangerObject & {
     readonly name: string,
     readonly thumbnail: string,
+    width: number,
+    readonly thickness: number,
+    height: number,
 }
